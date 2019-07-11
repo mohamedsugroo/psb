4226925bc6bbb0b6e3280bc5996f8f0364e4ada7
mohamedsugroo <mohamedsugroo@gmail.com>
2019-07-11 06:15:12 +0100

    connect controller

--- app/controllers/api/v1/connect/connect_controller.rb
+++ app/controllers/api/v1/connect/connect_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Connect::ConnectController < ApplicationController
-    before_action :authorize_request
+    # before_action :authorize_request
     def index
         render json: 123
     end
