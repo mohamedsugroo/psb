c2bd143758716851adfdd3379fd547019cbc8ed2
mohamedsugroo <mohamedsugroo@gmail.com>
2019-07-11 06:24:31 +0100

    connect roll back

--- app/controllers/api/v1/connect/connect_controller.rb
+++ app/controllers/api/v1/connect/connect_controller.rb
@@ -1,18 +1,8 @@
 class Api::V1::Connect::ConnectController < ApplicationController
-    before_action :connect_auth
+    # before_action :connect_auth
     def index
 
-        app = { name: "hello world", request_count: 600000 }
-
-        if connect_auth
-            render json: {
-                app: app
-            } 
-        else
-            render json: {
-                errors: 'unauthorized'
-            } 
-        end
+        render json: 'hello'
 
     end
 
