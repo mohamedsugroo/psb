2f6fb3078645b31a782b4d3e6d5aca16c96f49d7
mohamedsugroo <mohamedsugroo@gmail.com>
2019-07-12 04:56:50 +0100

    coffe

--- app/controllers/api/v1/connect/connect_controller.rb
+++ app/controllers/api/v1/connect/connect_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Connect::ConnectController < ApplicationController
     # before_action :connect_auth
     def index
 
-        app = { name: "hello world", request_count: 600000 }
+        app = { name: "hello world", request_count: 200000 }
         render json: {
             app: app
         } 
