7db6c8897a68498c9755db020d4fe01e89199b34
mohamedsugroo <mohamedsugroo@gmail.com>
2019-08-14 19:08:00 +0100

    payees

---
+++ app/controllers/api/v2/lib/profile.rb
@@ -0,0 +1,20 @@
+module Api::V2::Lib::Profiler
+
+	def self.users(users) 
+		users.each do |user|
+			# User.where(id: user.friend_id)
+			# profiled_user(newuser)
+			'user.friend_id'
+		end
+	end
+
+	private 
+		def profiled_user(user)
+			return {
+				username: user.username,
+				fullname: "#{user.first_name} #{user.middle_name} #{user.last_name}",
+				initial: user.first_name[0]
+			}
+		end
+
+end
